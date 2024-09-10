c08f9a1fef04bb40e3c36ba80be2dc2e0befe2d3
TarasDemchenko <tarasdemchenko1993@gmail.com>
2024-09-10 10:07:40 +0200

    hw.2

--- src/components/notification /Notification.jsx
+++ src/components/notification /Notification.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Notification = () => {
-  return <div>No feedback yet</div>;
+  return <p>No feedback yet</p>;
 };
 
 export default Notification;
--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import s from "./Options.module.css";
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
   return (
     <>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+      <div className={s.btn_group}>
+        <button className={s.btn} onClick={() => updateFeedback("good")}>
+          Good
+        </button>
+        <button className={s.btn} onClick={() => updateFeedback("neutral")}>
+          Neutral
+        </button>
+        <button className={s.btn} onClick={() => updateFeedback("bad")}>
+          Bad
+        </button>
+        {totalFeedback > 0 && (
+          <button className={s.btn_reset} onClick={resetFeedback}>
+            Reset
+          </button>
+        )}
+      </div>
     </>
   );
 };
